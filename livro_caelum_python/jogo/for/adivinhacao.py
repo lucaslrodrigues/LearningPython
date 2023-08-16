1bdea67383df6116c3747139c919e86f6291baa5
lucas <lucas.lrodrigues@sptech.shool>
2023-08-16 13:43:51 -0300

    atividades do livro

--- livro_caelum_python/jogo/for/adivinhacao.py
+++ livro_caelum_python/jogo/for/adivinhacao.py
@@ -4,10 +4,11 @@ def execute() -> str:
     print(f'%s\n\'    Jogo da adivinhação     \'\n%s' % (barra * 30, barra * 30))
 
     numero_secreto = 42
-    tentativas_restantes = 3
-    rodada = 1
+    partida = Partida()
+    partida.rodada(3)
+    print(partida.tentativas)
 
-    for i in range(1, tentativas_restantes + 1):
+    for i in range(1, partida.tentativas + 1):
         print(f'Tentativa %d de %d' % (rodada, tentativas_restantes))
         response = int(input("Qual é o número secreto: "))
         if response == numero_secreto:
@@ -25,6 +26,19 @@ def restam(tentativas: str) -> str:
 # def text() -> str:
 #     return ("---" * 15) + "Simple text"
 
+class Partida:
+    def __init__(self) -> None:
+        self.pontuacao: int = 1000
+        self.tentativas: int = 0
+
+    def rodada(self, nivel: int):
+        if nivel == 1:
+            self.tentativas = 20
+        elif nivel == 2:
+            self.tentativas = 10
+        else:
+            self.tentativas = 5
+ 
 
 if __name__ == "__main__":
     print(execute())
