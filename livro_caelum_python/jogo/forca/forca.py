ffb4a91006a1bb8b0d7cdf0f261f6aafd845b47a
lucas.rodrigues <lucas.rodrigues@yandeh.com.br>
2023-08-17 15:02:31 -0300

    atv do livro

--- livro_caelum_python/jogo/forca/forca.py
+++ livro_caelum_python/jogo/forca/forca.py
@@ -1,21 +1,26 @@
-def main():
-	palavra_secreta = "banana"
+from typing import List
 
-	letras_acertadas = []
+
+def main():
+	palavra_secreta: str = "banana"
+
+	letras_acertadas: List[str] = []
 	for letra in palavra_secreta:
-     		letras_acertadas.append("-")
-	terminou = False
+		letras_acertadas.append("-")
+	terminou: bool = False
 
 	while not terminou:
-    		chute = input("Qual letra")
-    		cont = 0
-    		for letra in palavra_secreta:
-         		if letra == palavra_secreta[cont]:
-	             		letras_acertadas[cont] = letra
-         	if list(palavra_secreta) == letras_acertadas:
-             	terminou == True
-         	cont += 1
-    	terminou = True
-    
+		print(f'Letras acertadas: {"".join(letras_acertadas)}') # join une a string vazia ás strings presentes na lista
+		chute = input("Qual letra: ")
+		cont: int = 0
+		for letra in palavra_secreta:
+			if chute == palavra_secreta[cont]:
+				letras_acertadas[cont] = letra
+			cont += 1
+		if list(palavra_secreta) == letras_acertadas:
+			terminou = True
+			print("Você acertou a palavra secreta")
+
+
 if __name__ == "__main__":
-    main()
+	main()
---
+++ livro_caelum_python/jogo/forca/test.py
@@ -0,0 +1,11 @@
+from typing import List
+
+palavra_secreta: str = "banana"
+
+letras_acertadas: List[str] = []
+for letra in palavra_secreta:
+    letras_acertadas.append("-")
+
+# p = [print(letra) for letra in letras_acertadas]
+
+print("".join(letras_acertadas))
