03f61d1df86cdb472fbaa9bf7aa2753014d50645
lucas <lucas.lrodrigues@sptech.shool>
2023-08-18 09:11:28 -0300

    atividades do livro

--- livro_caelum_python/jogo/forca/forca.py
+++ livro_caelum_python/jogo/forca/forca.py
@@ -7,19 +7,25 @@ def main():
 	letras_acertadas: List[str] = []
 	for letra in palavra_secreta:
 		letras_acertadas.append("-")
-	terminou: bool = False
-
+	terminou: bool = False;
+	erros: int = 0
 	while not terminou:
-		print(f'Letras acertadas: {"".join(letras_acertadas)}') # join une a string vazia ás strings presentes na lista
-		chute = input("Qual letra: ")
 		cont: int = 0
+		print(erros)
+		if erros >= 6:
+			return print("Você perdeu o jogo")
+		print(f'Letras acertadas: {"".join(letras_acertadas)}\n\
+Vidas restantes: {7 - cont} ') # join une a string vazia ás strings presentes na lista
+		chute = input("Qual letra: ")
+
 		for letra in palavra_secreta:
-			if chute == palavra_secreta[cont]:
+			if chute.upper() == palavra_secreta[cont].upper():
 				letras_acertadas[cont] = letra
+				
 			cont += 1
 		if list(palavra_secreta) == letras_acertadas:
 			terminou = True
-			print("Você acertou a palavra secreta")
+			print(f'Você acertou a palavra secreta %s' % (palavra_secreta))
 
 
 if __name__ == "__main__":
